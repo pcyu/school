4b2c3684a7befaea08c2112041c56cf53309c109
root <root@localhost.localdomain>
2018-10-29 16:21:12 +0800

    fizzbuzz is misspelled

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function helloWorld(message) {
 }
 
 // for (let i=1; i<101; i++) {
-//   i%15 === 0 ? console.log("fizzbuzz") : i%5 === 0 ? console.log('buzz') : i%3 === 0 ? console.log('fizz') : console.log(i);
+//   i%15 === 0 ? console.log("fifzzbuzz") : i%5 === 0 ? console.log('buzz') : i%3 === 0 ? console.log('fizz') : console.log(i);
 // }
 
 helloWorld("hi");
