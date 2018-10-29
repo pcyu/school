d4ef46b10dbdd6c9532c1a7f5ce8b6df8b6b42c3
Peter Yu <peter.y@nais.com>
2018-10-29 14:02:25 +0800

    changed function to use a paramater

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-function helloWorld() {
-    console.log("hello world");
+function helloWorld(message) {
+    console.log(message);
 }
 
-helloWorld();
+// for (let i=1; i<101; i++) {
+//   i%15 === 0 ? console.log("fizzbuzz") : i%5 === 0 ? console.log('buzz') : i%3 === 0 ? console.log('fizz') : console.log(i);
+// }
 
-const printName = (name) => {
-    console.log(name);
-};
-
-printName('Peter Yu');
+helloWorld("hi");
